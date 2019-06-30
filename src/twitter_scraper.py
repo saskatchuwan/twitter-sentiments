f30eb40a246788efc47e9c76228a39dea4d9b9bd
Kathryn <kathryn.chu10@gmail.com>
2019-06-30 16:37:08 -0700

    Add new functionality to scraper class to scrape more tweets from a user profile / search result page

--- src/twitter_scraper.py
+++ src/twitter_scraper.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import json
 
 import requests
 import sys
@@ -10,21 +11,35 @@ import tweet
 
 class TwitterScraper( object ):
 
+  def __init__( self, 
+                page_url_base="https://twitter.com/search?f=tweets&lang=en&q=", 
+                next_url_base="https://twitter.com/i/search/timeline?f=tweets&src=typd&q=", 
+                next_url_additional='',
+                content_type='search'
+              ):
+    self.page_url_base = page_url_base
+    self.next_url_base = next_url_base
+    self.next_url_additional = next_url_additional
+    self.content_type = content_type
+
   def perform_scrape(self, query_term):
     print('query_term received ' + query_term)
-    url = f"https://twitter.com/search?f=tweets&q={query_term}&lang=en"
-    headers = proxy.get_headers()
-    print(headers)
+    url = self.page_url_base + query_term
 
-    response = requests.get(url, headers=headers)
+    response = self.request(url)
     print("-- STATUS " + str(response.status_code) + " -- " + url)
+
+    tweets_list = list()
     if response.status_code == 200:
-      self.parse_tweets(response, 'search')
+      soup = BeautifulSoup(response.text, 'lxml')
+      tweets_list.extend(self.parse_tweets(soup, self.content_type))
+
+      tweets_list.extend(self.get_more_tweets(soup, query_term))
+
+    return tweets_list
 
 
-  def parse_tweets(self, response_obj, content_type):
-    soup = BeautifulSoup(response_obj.text, 'lxml')
-
+  def parse_tweets(self, soup, content_type):
     tweets_list = list()
     tweets = soup.find_all("li", {"data-item-type": "tweet"})
 
@@ -33,6 +48,40 @@ class TwitterScraper( object ):
         tweets_list.append(tweet_text)
 
     print(str(len(tweets_list)) + " tweets found.")
-    print(tweets_list)
     return tweets_list
 
+  def get_more_tweets(self, soup, query_term):
+    next_pointer = soup.find("div", {"class": "stream-container"})["data-min-position"]
+    tweets_list = list()
+
+    extra_fetches = 0
+    while extra_fetches <= 5:
+        next_url = self.next_url_base + query_term + self.next_url_additional + '&max_position=' + next_pointer
+  
+        print(next_url)
+        next_response = None
+        try:
+            next_response = self.request(next_url)
+        except Exception as e:
+            # in case there is some issue with request. None encountered so far.
+            print(e)
+            return tweets_list
+
+        tweets_data = next_response.text
+        tweets_obj = json.loads(tweets_data)
+
+        if not tweets_obj["has_more_items"] and not tweets_obj["min_position"]:
+            print("\nNo more tweets returned")
+            break
+
+        next_pointer = tweets_obj["min_position"]
+        html = tweets_obj["items_html"]
+        soup = BeautifulSoup(html, 'lxml')
+        tweets_list.extend(self.parse_tweets(soup, self.content_type))
+        extra_fetches += 1
+    return tweets_list
+
+  def request(self, url):
+    headers = proxy.get_headers()
+    response = requests.get(url, headers=headers)
+    return response
--- src/user_scraper.py
+++ src/user_scraper.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import json
 
 import requests
 import sys
@@ -10,12 +11,29 @@ from twitter_scraper import TwitterScraper
 
 class UserScraper( TwitterScraper ):
 
+  def __init__( self, 
+                page_url_base="https://twitter.com/", 
+                next_url_base="https://twitter.com/i/profiles/show/", 
+                next_url_additional="/timeline/tweets?include_available_features=1&include_entities=1&reset_error_state=false",
+                content_type='user'
+              ):
+    self.page_url_base = page_url_base
+    self.next_url_base = next_url_base
+    self.next_url_additional = next_url_additional
+    self.content_type = content_type
+
   def perform_scrape(self, handle):
     print('handle received ' + handle)
-    url = "https://twitter.com/" + handle
-    headers = proxy.get_headers()
+    url = self.page_url_base + handle
 
-    response = requests.get(url, headers=headers)
+    response = TwitterScraper.request(self, url)
     print("-- STATUS " + str(response.status_code) + " -- " + url)
+
+    tweets_list = list()
     if response.status_code == 200:
-        TwitterScraper.parse_tweets(self, response, 'user')
+        soup = BeautifulSoup(response.text, 'lxml')
+        tweets_list.extend(TwitterScraper.parse_tweets(self, soup, self.content_type))
+
+        tweets_list.extend(TwitterScraper.get_more_tweets(self, soup, handle))
+
+    return tweets_list
