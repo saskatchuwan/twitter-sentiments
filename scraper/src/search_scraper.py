96d1c463fe24c87a333c824f34ee137e1c33e301
Kathryn <kathryn.chu10@gmail.com>
2019-09-22 09:31:27 -0700

    Remove extraneous init files and structural changes

--- scraper/src/search_scraper.py
+++ scraper/src/search_scraper.py
@@ -2,8 +2,6 @@ from bs4 import BeautifulSoup
 
 import requests
 import sys
-sys.path.insert(0, '/Users/kathrynchu/src/twitter-sentiments')
-import proxy
 
 import tweet
 from twitter_scraper import TwitterScraper
